32084b7a7be77e9996dc7331c6dcb0f6b1b7c681
lvivtotoro <7090833+lvivtotoro@users.noreply.github.com>
2018-05-21 21:21:23 +0300

    Forgot to init.

--- test.cpp
+++ test.cpp
@@ -3,6 +3,7 @@
 #include"copit.h"
 
 int main() {
-    printf("10 things. %s%s11", copit::line_return(), copit::fg(copit::red));
+    copit::init();
+    printf("10 things. %s%s11", copit::line_return(), copit::bg(copit::aqua));
     return 0;
 }
