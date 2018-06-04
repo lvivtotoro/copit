dde6cc0a23fffb86fbca039da112064b5c23061a
lvivtotoro <7090833+lvivtotoro@users.noreply.github.com>
2018-06-05 00:02:33 +0300

    Updated test.cpp.

--- test.cpp
+++ test.cpp
@@ -1,9 +1,15 @@
 #include<stdio.h>
+#include<iostream>
 
 #include"copit.h"
 
 int main() {
     copit::init();
-    printf("10 things. %s%s11", copit::line_return(), copit::bg(copit::aqua));
+
+    unsigned short x, y;
+    copit::getsize(&x, &y);
+    std::cout << x << " " << y;
+    fflush(stdout);
+
     return 0;
 }
